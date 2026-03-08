0acc66011832e7ed2cd41e75e4d2afdc69b1162c
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import ContributorCard from '../components/ContributorCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Contributors | NovaCMS',
--- app/features/page.tsx
+++ app/features/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import FeatureCard from '../components/FeatureCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Features | NovaCMS',
